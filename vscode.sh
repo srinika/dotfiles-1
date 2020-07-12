dc832d1682ec406d1cf103cf4cf2955111cd6c87
Srinika Gunawardane <srinikagunawardane@gmail.com>
2020-07-12 15:38:26 +1200

    update the vscode.sh with extension installation via cli

--- vscode.sh
+++ vscode.sh
@@ -6,3 +6,16 @@ mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\
 
 # Set symlink to custom user settings
 ln -sf ~/dotfiles/settings/vscode/VSCode-Settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+# Install extensions
+code --install-extension amazonwebservices.aws-toolkit-vscode
+code --install-extension aws-amplify.aws-amplify-vscode
+code --install-extension aws-scripting-guy.cform
+code --install-extension erd0s.terraform-autocomplete
+code --install-extension hashicorp.terraform
+code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension ms-python.python
+code --install-extension PKief.material-icon-theme
+code --install-extension redhat.vscode-yaml
+code --install-extension sleutho.templex
+code --install-extension teabyii.ayu
